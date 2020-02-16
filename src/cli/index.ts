1e371e5d4b4d73f533f24604c4ae1a84b03e1a44
Ovyerus <iamovyerus@gmail.com>
2020-02-16 23:30:16 +1100

    feat: add CLI tool

---
+++ src/cli/index.ts
@@ -0,0 +1,112 @@
+#!/usr/bin/env node
+import ConfigStore from 'configstore';
+import gitPath from 'git-config-path';
+import { sync as parseGitConfig } from 'parse-git-config';
+import { all } from 'rambda';
+import yargs from 'yargs';
+
+import fs from 'fs';
+import path from 'path';
+
+import config from './config';
+import run from './run';
+
+// eslint-disable-next-line no-sync
+const pkg = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../../package.json'), 'utf-8')
+);
+const gitConfig = () => parseGitConfig({ cwd: '/', path: gitPath('global') });
+
+const getUserName = (): string => cfg.get('name') || gitConfig().user.name;
+const getUserEmail = (): string => cfg.get('email') || gitConfig().user.email;
+
+const validateYear = (year: string) => {
+  const split = year.split('-');
+
+  if (
+    /^\d{4}$/.test(year) ||
+    (split.length === 2 && all(x => /^\d{4}$/.test(x), split)) ||
+    /^(?:\d{4}(?:, ?)?)+$/.test(year)
+  )
+    return year;
+  else
+    throw new Error(
+      'Year should either be a standalone year `2020`, a year range `2019-2020`, or a list of years `1999, 2003, 2005`'
+    );
+};
+
+export const cfg = new ConfigStore(pkg.name);
+
+// if (something) {
+main();
+// }
+
+export default function main() {
+  return yargs
+    .scriptName(pkg.name)
+    .version(pkg.version)
+    .usage('todo')
+    .command(
+      '$0 [license]',
+      'Generate a license file.',
+      yargs => {
+        yargs
+          .positional('license', {
+            type: 'string',
+            default: cfg.get('license') || null,
+            describe: 'License to use'
+          })
+          .option('n', {
+            alias: 'name',
+            default: getUserName(),
+            describe: "The copyright owner's name.",
+            type: 'string'
+          })
+          .option('e', {
+            alias: 'email',
+            default: getUserEmail(),
+            describe: "The copyright owner's email address."
+          })
+          .option('y', {
+            alias: 'year',
+            default: new Date().getFullYear().toString(),
+            describe: 'The copyright year.',
+            type: 'string'
+          })
+          .option('raw', {
+            default: false,
+            describe: "Print out the license's template.",
+            type: 'boolean'
+          })
+          .option('non-osi', {
+            default: false,
+            describe:
+              'Whether to show non-OSI approved licenses in the license prompt.',
+            type: 'boolean'
+          })
+          .coerce('year', validateYear);
+      },
+      run
+    )
+    .command(
+      'config',
+      'Set and view config',
+      yargs => {
+        yargs
+          .option('name', {
+            describe: 'Name to use by default.',
+            type: 'string'
+          })
+          .option('email', {
+            describe: 'Email to use by default.',
+            type: 'string'
+          })
+          .option('license', {
+            describe: 'License to use if none is specified.',
+            type: 'string'
+          });
+      },
+      config
+    )
+    .help().argv;
+}
---
+++ src/cli/run.ts
@@ -0,0 +1,89 @@
+import licenses, { Identifiers } from '@ovyerus/licenses';
+import identifiers from '@ovyerus/licenses/simple';
+import prompts from 'prompts';
+
+import { promises as fs } from 'fs';
+
+import getLicense from '../getLicense';
+
+const osiOnlyIdentifiers = Object.entries(licenses)
+  .filter(([, { osiApproved }]) => osiApproved)
+  .map(([identifier]) => identifier);
+
+interface RunArgv {
+  license?: Identifiers[number];
+  name: string;
+  email: string;
+  year: string;
+  raw: boolean;
+  nonOsi: boolean;
+}
+
+export default async function run({
+  license: license_,
+  name,
+  email,
+  year,
+  // TODO: other fields for other placeholders in @ovyerus/licenses
+  raw,
+  nonOsi
+}: RunArgv) {
+  if (raw) {
+    if (!license_) throw new Error('Specify what license to print');
+    if (!identifiers.has(license_))
+      throw new Error('license must be a valid SPDX identifier');
+
+    const { licenseText } = JSON.parse(
+      await fs.readFile(
+        require.resolve(`@ovyerus/licenses/licenses/${license_}`),
+        'utf-8'
+      )
+    );
+
+    process.stdout.write(licenseText);
+    process.exit(0);
+  }
+
+  let license = license_;
+  let filename = 'LICENSE';
+  const currentLicense = (await fs.readdir('./')).find(x =>
+    /^LICENSE(\.md|\.txt)?$/i.test(x)
+  ); // Check if an existing LICENSE file already exists
+
+  if (currentLicense) {
+    console.log("Looks like there's already a license file for this project.");
+
+    const { replaceLicense }: { replaceLicense: boolean } = await prompts({
+      name: 'replaceLicense',
+      type: 'confirm',
+      message: 'Do you want to replace it?'
+    });
+
+    if (replaceLicense) filename = currentLicense;
+    else return console.log('Exiting...');
+  }
+
+  if (!license || !identifiers.has(license)) {
+    const response = await prompts({
+      name: 'license',
+      type: 'autocomplete',
+      message: 'Search for the license you want',
+      choices: (nonOsi ? Array.from(identifiers) : osiOnlyIdentifiers).map(
+        x => ({ title: x, value: x })
+      )
+    });
+
+    ({ license } = response);
+    return;
+  }
+
+  const text = getLicense(license, { name, year, email });
+
+  await fs.writeFile(`./${filename}`, text);
+
+  console.log(`Successfully wrote the ${license} license to ./LICENSE`);
+  console.log(
+    'Most information *should* have been updated with your details but ' +
+      'it is best to double check to make sure it is all correct.'
+  );
+}
